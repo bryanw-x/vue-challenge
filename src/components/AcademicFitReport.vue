70dbf48e732a12b23b66d16ccb0276ab746a9f67
Bryan <bryanwong92@gmail.com>
2023-07-02 00:26:14 -0400

    Fit Report Header Row
    Some cleanup as well

--- src/components/AcademicFitReport.vue
+++ src/components/AcademicFitReport.vue
@@ -1,51 +1,67 @@
 <template>
   <div class="container">
-        <div class="header">
-          <i class="profile-picture">profile_image</i>
-          <h2 class="athlete-name">{{athlete.name}}</h2>
+    <div class="header">
+      <i class="profile-picture">profile_image</i>
+      <h2 class="athlete-name">{{athlete.name}}</h2>
+    </div>
+    <div class="student-info">
+      <div class="data">
+        <ul>
+          <li>
+            <label>Sport:</label>
+            {{athlete.sport}}
+          </li>
+          <li>
+            <label>Class:</label>
+            {{athlete.grad_year}}
+          </li>
+          <li>
+            <label>Club:</label>
+            {{athlete.club.name}}
+          </li>
+        </ul>
+      </div>
+      <div class="data">
+        <ul>
+          <li>
+            <label>High School:</label>
+            {{athlete.high_school.name}}
+          </li>
+          <li>
+            <label>GPA:</label>
+            {{athlete.gpa}}
+          </li>
+          <li>
+            <label>Desired Major:</label>
+            {{athlete.major}}
+          </li>
+        </ul>
+      </div>
+      <div class="logo-section">
+        <div class="logo">
+          <img :src="require('@/assets/Asset_1.png')" alt="Logo Image">
         </div>
-        <div class="student-info">
-          <div class="data">
-            <ul>
-              <li>
-                <label>Sport:</label>
-                {{athlete.sport}}
-              </li>
-              <li>
-                <label>Class:</label>
-                {{athlete.grad_year}}
-              </li>
-              <li>
-                <label>Club:</label>
-                {{athlete.club.name}}
-              </li>
-            </ul>
-          </div>
-          <div class="data">
-            <ul>
-              <li>
-                <label>High School:</label>
-                {{athlete.high_school.name}}
-              </li>
-              <li>
-                <label>GPA:</label>
-                {{athlete.gpa}}
-              </li>
-              <li>
-                <label>Desired Major:</label>
-                {{athlete.major}}
-              </li>
-            </ul>
-          </div>
-          <div class="logo-section">
-            <div class="logo">
-              <img :src="require('@/assets/Asset_1.png')" alt="Logo Image">
-            </div>
-            <div class="logo-text">
-              <p>Academic Fit Report</p>
-            </div>
-          </div>
+        <div class="logo-text">
+          <p>Academic Fit Report</p>
         </div>
+      </div>
+    </div>
+    <div class="fit-report-header">
+      <table>
+        <thead>
+          <tr>
+            <th>School Name</th>
+            <th>Athletic Div</th>
+            <th>Conference</th>
+            <th>Ranking*<br>(DI NCAA)<br>(DII & DIII Hero Sports)</th>
+            <th>GPA**<br>(Min 25% 50% 75% Max)</th>
+            <th>SAT Reding***<br>25%-75%</th>
+            <th>SAT Math***<br>25%-75%</th>
+            <th>ACT Composite***<br>25%-75%</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
   </div>
 </template>
 
