7b25869f0c19bfc0ffd1f3f1fa904de6d8cac4d4
Bryan <bryanwong92@gmail.com>
2023-07-02 05:59:46 -0400

    Added gpa color Component

---
+++ src/components/GPAColorCode.vue
@@ -0,0 +1,37 @@
+<template>
+  <td :style="{ backgroundColor: gpaColor }">{{ gpa }}</td>
+</template>
+
+<script>
+export default {
+ name: "GPAColorCode",
+ props: {
+   gpa: {
+     type: Number,
+     required: true,
+   },
+ },
+ data() {
+   return {
+     userGpa: 3.8
+   };
+ },
+ computed: {
+   gpaColor() {
+     const difference = this.gpa - this.userGpa;
+
+     if (difference > 0.1) {
+       return "#d7737d";
+     } else if (difference > 0 && difference <= 0.1) {
+       return "#c194b5";
+     } else if (difference === 0) {
+       return "#b4a7d6";
+     } else if (difference < 0 && difference >= -0.1) {
+       return "#a6a8da";
+     } else {
+       return "#75ace5";
+     }
+   }
+ }
+};
+</script>
